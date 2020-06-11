028fd8c9ff7b5d86ddc08c67caa979221234d6d8
karleighponnock <karleighponnock@gmail.com>
2020-06-10 20:43:51 -0400

    array for criteria is being requested and filled

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+  // Build array for criteria
+var  passCriteria = [];
+
+  // Prompt user for password criteria
+//TODO can only be between 8 and 128 characters
+  var numOfChar = prompt("How many characters do you want in your password?");
+
+  var lowerCase = confirm("Include lowercase letters?");
+
+  var upperCase = confirm("Include uppercase letters?");
+
+  var numbers= confirm("Include numbers?");
+
+  var specialChar= confirm("Include special characters?");
+ 
+passCriteria.push(numOfChar,lowerCase,upperCase,numbers,specialChar);
+
+
+
+  return "This is the Password"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,4 +35,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+//calls for function above to be run
 generateBtn.addEventListener("click", writePassword);
