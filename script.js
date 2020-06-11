e40e19f46dee4ad7bb6830a44af6b6d0656d079d
karleighponnock <karleighponnock@gmail.com>
2020-06-11 15:33:13 -0400

    adding notes in

--- script.js
+++ script.js
@@ -16,39 +16,52 @@ function generatePassword() {
   var numOfChar = prompt("How many characters do you want in your password?");
   if (numOfChar > 8 && numOfChar < 128) {
 
+    // ask if user ants to include lowercase letters
     var lowerCase = confirm("Include lowercase letters?");
 
+    // ask if user ants to include uppercase letters
     var upperCase = confirm("Include uppercase letters?");
 
+    // ask if user ants to include numbers
     var numbers = confirm("Include numbers?");
 
+    // ask if user ants to include special characters
     var specialChar = confirm("Include special characters?");
 
-
+    // If used confirmed use of lowercase letters this will 
+    // push the contents of lowerCharArray into the criteria array
     if (lowerCase) {
       passCriteria.push(lowerCharArray);
     }
     console.log(passCriteria);
 
+    // If used confirmed use of uppercase letters this will 
+    // push the contents of upperCase array into the criteria array
     if (upperCharArray) {
       passCriteria.push(upperCase);
     }
     console.log(passCriteria);
 
+
+    // If used confirmed use of numbers this will 
+    // push the contents of numArray into the criteria array
     if (numbers) {
       passCriteria.push(numArray);
     }
     console.log(passCriteria);
 
+    // If used confirmed use of special characters this will 
+    // push the contents of specialCharactersArray into the criteria array
     if (specialChar) {
       passCriteria.push(specialCharactersArray);
     }
 
-console.log(passCriteria);
+    // not needed for functionality, visual of all criteria for developer
+    console.log(passCriteria);
 
-  
 
-return "This is the Password"
+
+    return "This is the Password"
 
   } else {
     alert("Must be between 8 and 128 character. Press Generate to try again!")
