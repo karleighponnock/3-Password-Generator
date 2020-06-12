2b875e2268aa9f9269e39f68efbfe7ae010bb968
karleighponnock <karleighponnock@gmail.com>
2020-06-12 10:03:34 -0400

    final product

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function generatePassword() {
 
   // Build array for criteria
   var passCriteria = [];
-
+  var result = []
 
   // Prompt user for password criteria
   var numOfChar = prompt("How many characters do you want in your password?");
@@ -32,14 +32,14 @@ function generatePassword() {
     // If used confirmed use of lowercase letters this will 
     // push the contents of lowerCharArray into the criteria array
     if (lowerCase) {
-      passCriteria.push(lowerCharArray);
+      passCriteria = passCriteria.concat(lowerCharArray);
     }
     // console.log(passCriteria);
 
     // If used confirmed use of uppercase letters this will 
     // push the contents of upperCase array into the criteria array
-    if (upperCharArray) {
-      passCriteria.push(upperCase);
+    if (upperCase) {
+      passCriteria = passCriteria.concat(upperCharArray);
     }
     // console.log(passCriteria);
 
@@ -47,23 +47,26 @@ function generatePassword() {
     // If used confirmed use of numbers this will 
     // push the contents of numArray into the criteria array
     if (numbers) {
-      passCriteria.push(numArray);
+      passCriteria = passCriteria.concat(numArray);
     }
     // console.log(passCriteria);
 
     // If used confirmed use of special characters this will 
     // push the contents of specialCharactersArray into the criteria array
     if (specialChar) {
-      passCriteria.push(specialCharactersArray);
+      passCriteria = passCriteria.concat(specialCharactersArray);
+    }
+// Stop program if user did not select any options
+    if (passCriteria.length === 0) {
+      return alert("You must select at least one of the criteria options! Try again.");
     }
 
-    // not needed for functionality, visual of all criteria for developer
-    return passCriteria.join("").replace(/,/g,"");
-
-
-
-  
-
+    for (var i = 0; i < numOfChar; i++) {
+      var randomChar = getRandom(passCriteria);
+    
+      result.push(randomChar);
+    }
+    return result.join("");
 
     // if user inputs a number not within the length 
     // parameters 8-128 they are prompted to try again
@@ -73,10 +76,20 @@ function generatePassword() {
   }
 }
 
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+
+  return randElement;
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+
 
   passwordText.value = password;
 
